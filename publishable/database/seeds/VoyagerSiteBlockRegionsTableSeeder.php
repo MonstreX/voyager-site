e77e3b77d045be514147b36b765a65a27fab7a66
monstrex <fido6080net@gmail.com>
2020-04-28 11:23:23 +0600

    fixes for v0.84 #4

--- publishable/database/seeds/VoyagerSiteBlockRegionsTableSeeder.php
+++ publishable/database/seeds/VoyagerSiteBlockRegionsTableSeeder.php
@@ -11,35 +11,35 @@ class VoyagerSiteBlockRegionsTableSeeder extends Seeder
     public function run()
     {
         $region = Region::firstOrCreate([
-            'title' => __('voyager-site::regions.content_before'),
+            'title' => __('voyager-site::seeders.regions.content_before'),
             'key' => 'content-before',
             'order' => 1,
             'color' => '#2f8516',
         ]);
 
         $region = Region::firstOrCreate([
-            'title' => __('voyager-site::regions.content'),
+            'title' => __('voyager-site::seeders.regions.content'),
             'key' => 'content',
             'order' => 2,
             'color' => '#2f8516',
         ]);
 
         $region = Region::firstOrCreate([
-            'title' => __('voyager-site::regions.content_after'),
+            'title' => __('voyager-site::seeders.regions.content_after'),
             'key' => 'content-after',
             'order' => 3,
             'color' => '#4bc2a2',
         ]);
 
         $region = Region::firstOrCreate([
-            'title' => __('voyager-site::regions.no_position'),
+            'title' => __('voyager-site::seeders.regions.no_position'),
             'key' => 'no-position',
             'order' => 4,
             'color' => '#c7c7c7',
         ]);
 
         $region = Region::firstOrCreate([
-            'title' => __('voyager-site::regions.sidebar'),
+            'title' => __('voyager-site::seeders.regions.sidebar'),
             'key' => 'sidebar',
             'order' => 5,
             'color' => '#e20a0f',
